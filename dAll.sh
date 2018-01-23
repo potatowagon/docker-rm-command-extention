dd07e0807c54a8397f15889ca6a6cb8c0c9a6ea4
ADS\t_wongsh <sherry.wong.wen.qi@autodesk.com>
2018-01-23 12:07:04 +0800

    bug fix elif -> else

--- dAll.sh
+++ dAll.sh
@@ -5,24 +5,24 @@ cmdName="dAll"
 dockerContainers1=$(docker container ls -all | awk '{print $1}')
 toDelete="CONTAINER"
 dockerContainers2=${dockerContainers1#$toDelete}
-   
+
 if [ "$1" == "" ]
 then    
-    docker container rm $dockerContainers2
+	docker container rm $dockerContainers2
 else
 	if [ $# > 2 ]
 	then
 		echo "Invalid argument. Try \n./$cmdName.sh\nRemove all containers\n\n./$cmdName.sh <image>\nRemove all conatiners of <image>\n\n$./$cmdName.sh -e <image>\nRemove all containers except for containers with <image>" 
 	else
-                imageString=$(docker container ls -all | awk '{print $2}')
-                IFS=" " read -r -a imageArr <<< "$imageString"
-                read -r -a dockerContainersArr <<< "$dockerContainers1"
-	
+		imageString=$(docker container ls -all | awk '{print $2}')
+		IFS=" " read -r -a imageArr <<< "$imageString"
+		read -r -a dockerContainersArr <<< "$dockerContainers1"
+
 		if [ "$1" == "-e" ]
 		then
 			shift
 			delExcept=$1
-                	for i in "${!imageArr[@]}"
+			for i in "${!imageArr[@]}"
 			do
 				if [ "${imageArr[i]}" == "$delExcept" ]
 				then
@@ -32,8 +32,8 @@ else
 			unset "dockerContainerArr[0]" #remove the string "CONTAINER"
 			dockerConatinersToDel=$( IFS=$" "; echo "${dockerContainersArr[*]}" )
 			docker container rm $dockerConatinerToDel
-		elif
-		        del=$1
+		else
+			del=$1
 			dockerContainersToDel=""
 			for i in "${!imageArr[@]}"
 			do
@@ -46,4 +46,4 @@ else
 		fi
 	fi
 fi
-				
+
